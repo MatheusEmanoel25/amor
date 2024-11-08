78be15fac37a4c7386755e3c9bd408b573782aea
MatheusEmanoel25 <matheusemanoeldsp@gmail.com>
2024-11-07 22:37:51 -0300

    futuro

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    // Acha o container da mensagem
+    const messageContainer = document.getElementById("message-container");
+  
+    // Torna visível o container com a imagem e mensagem
+    messageContainer.style.display = "block";
+  }
+  
